8b45caf8c65065a83ce9494293daeabd776fe007
!averygolf! <https://github.com/shettyarun/epaamOrderDemo.git>
2021-07-14 20:29:36 +0530

    final changes

---
+++ epaamOrderDemo/src/main/java/com/epaam/rest/model/Item.java
@@ -0,0 +1,57 @@
+package com.epaam.rest.model;
+
+public class Item {
+
+	private String name;
+	private String category;
+	private Double price;
+	private Double totalPrice;
+	private int quantity;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(Double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [name=" + name + ", category=" + category + ", price=" + price + ", totalPrice=" + totalPrice
+				+ ", quantity=" + quantity + "]";
+	}
+
+}
